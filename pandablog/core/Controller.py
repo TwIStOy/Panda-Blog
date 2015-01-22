fa1b382d6ae3506edb6a54b9bacb3c3ad9290f36
Charlie.Lin <mishanlfq@gmail.com>
2015-01-22 15:59:18 +0800

    add Config.py to handle global_config

--- pandablog/core/Controller.py
+++ pandablog/core/Controller.py
@@ -4,20 +4,13 @@ __author__ = 'TwIStOy'
 import Loader
 import Selector
 import Renderer
+import Config
 import Util as util
-import os
-import json
-import codecs
 
 
 class Controller(object):
     def __init__(self, root):
-        if os.path.isfile(util.get_path(root, 'config.json')):
-            with codecs.open(util.get_path(root, 'config.json'), "r", encoding='utf-8') as fp:
-                global_config = json.load(fp)
-        else:
-            raise OSError('No "config.json" file! Please check and retry.')
-        self.global_config = global_config
+        self.global_config = Config.load_config(root)
         self.loader = Loader.Loader(util.get_path(root, 'src'), global_config)
         self.selector = Selector.Selector(root)
         self.renderer = Renderer.Renderer(root, global_config)
